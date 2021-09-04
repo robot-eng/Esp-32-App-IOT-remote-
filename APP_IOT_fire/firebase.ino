a783d214d12d4d33b0b87b20c2314857cea83e3c
Watcahanan <79349006+robot-eng@users.noreply.github.com>
2021-09-04 19:49:16 +0700

    Add files via upload

---
+++ APP_IOT_fire/firebase.ino
@@ -0,0 +1,137 @@
+
+#include "FirebaseESP32.h"	
+#include "WiFi.h"
+#include "Magellan.h"
+#include <Adafruit_Sensor.h>
+#include <DHT.h>		
+
+
+#define FIREBASE_HOST "xxxxxxxxxxxxxxx"
+#define FIREBASE_AUTH "xxxxxxxxxxxxxxxxxxx"
+#define WIFI_SSID "xxxxxxxx"
+#define WIFI_PASSWORD "xxxxxxxxxxxxxxxxx"
+
+#define DHTPIN 23		
+int led = 5;			
+
+#define DHTTYPE    DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+//Define FirebaseESP8266 data object
+FirebaseData firebaseData;
+FirebaseData ledData;
+
+FirebaseJson json;
+
+
+void setup()
+{
+
+  Serial.begin(9600);
+
+  dht.begin();
+  pinMode(led,OUTPUT);
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+}
+
+void sensorUpdate(){
+ 
+  float h = dht.readHumidity();
+
+  float t = dht.readTemperature();
+
+  float f = (1.80*(t) + 32);
+
+
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C  ,"));
+  Serial.print(F("% Fahrenheit: "));
+  Serial.print(f);
+  Serial.print(F("°F  ,"));
+
+  if (Firebase.setFloat(firebaseData, "/firebaseiot/temperature", t))
+  {
+    Serial.println("PASSED");
+    Serial.println("PATH: " + firebaseData.dataPath());
+    Serial.println("TYPE: " + firebaseData.dataType());
+    Serial.println("ETag: " + firebaseData.ETag());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+  else
+  {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+if (Firebase.setFloat(firebaseData, "/firebaseiot/Fahrenheit", f))
+  {
+    Serial.println("PASSED");
+    Serial.println("PATH: " + firebaseData.dataPath());
+    Serial.println("TYPE: " + firebaseData.dataType());
+    Serial.println("ETag: " + firebaseData.ETag());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+  else
+  {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+  if (Firebase.setFloat(firebaseData, "/firebaseiot/humidity", h))
+  {
+    Serial.println("PASSED");
+    Serial.println("PATH: " + firebaseData.dataPath());
+    Serial.println("TYPE: " + firebaseData.dataType());
+    Serial.println("ETag: " + firebaseData.ETag());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+  else
+  {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+}
+void loop() {
+  sensorUpdate();
+  
+  if (Firebase.getString(ledData, "/firebaseiot/led")){
+    Serial.println(ledData.stringData());
+    if (ledData.stringData() == "1") {
+    digitalWrite(led, HIGH);
+    }
+  else if (ledData.stringData() == "0"){
+    digitalWrite(led, LOW);
+    }
+  }
+  delay(100);
+}
